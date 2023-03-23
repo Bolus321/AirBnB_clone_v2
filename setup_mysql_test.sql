2e1f138e186fe3bb5b2ff051b0cd43e2518afac0
Bolus321 <nnamdiogbolu321@gmail.com>
2023-03-22 20:20:45 -0700

    AirBnB_clone_v2_B

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,6 @@
--- a script that prepares a MySQL server for the project
+-- A script that Prepares a MySQL Test server for the project.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES on hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
-SET FOREIGN_KEY_CHECKS=1;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
